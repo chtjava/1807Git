59f7e41ca2dbe8b4138248016c15d4c20b202218
chtjava <chtjava@163.com>
2019-06-12 14:16:53 +0800

    down commit

--- src/main/java/com/baidu/test/Student.java
+++ src/main/java/com/baidu/test/Student.java
@@ -2,6 +2,6 @@ package com.baidu.test;
 
 public class Student {
    public static void main(String[] args) {
-	System.out.println("left Join and join");
+	System.out.println("down Join and join");
  }
 }
